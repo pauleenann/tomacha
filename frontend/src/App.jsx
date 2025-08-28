eb4cd5a5ef2a506b0c9bd7cad84d66be24777cc6
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-08-28 16:05:14 +0800

    add protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Login from './pages/Login/Login'
+import { ProtectedRoutes } from './routes/ProtectedRoutes'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          {/* public routes */}
           <Route
           path='/login'
           element={<Login/>}
           />
+
+          {/* protected routes */}
+          <Route element={<ProtectedRoutes/>}>
+            <Route
+            path='/'
+            element={<Home/>}
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,12 @@
+
+import { Navigate, Outlet } from "react-router";
+import { useAuth } from "../context/AuthContext"
+
+export const ProtectedRoutes = ()=>{
+    const {accessToken} = useAuth();
+
+    return (
+        // <Outlet /> is basically the slot where child routes get injected inside a parent route’s layout.
+        accessToken ? <Outlet/> : <Navigate to='/login'/>
+    )
+}
